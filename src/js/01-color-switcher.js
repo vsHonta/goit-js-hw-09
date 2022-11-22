0cac3867679278c39f8e83eb3748808e9c464f5f
Viktor H <66110545+vsHonta@users.noreply.github.com>
2022-11-22 17:58:42 -0600

    1st Done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+const refs = {
+    startBtn: document.querySelector('button[data-start]'),
+    stopBtn: document.querySelector('button[data-stop]'),
+    body: document.querySelector('body'),
+}
 
+let timerId = null
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function onStartClick() {
+    const repaint = () => refs.body.style.backgroundColor = getRandomHexColor()
+    timerId = setInterval(repaint, 1000)
+    refs.startBtn.disabled = true
+}
+
+function onStopClick() { 
+    clearInterval(timerId)
+    refs.startBtn.disabled = false
+}
+
+refs.startBtn.addEventListener('click', onStartClick)
+refs.stopBtn.addEventListener('click', onStopClick)
